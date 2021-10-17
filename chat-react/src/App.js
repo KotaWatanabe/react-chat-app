769bcf9019d78680f769d214f6019830775457d0
Kota Watanabe <kota3316@gmail.com>
2021-10-17 16:42:37 -0700

    basic chant engine

--- chat-react/src/App.js
+++ chat-react/src/App.js
@@ -1,3 +1,17 @@
 import { ChatEngine } from "react-chat-engine";
-
+import ChatFeed from "./components/ChatFeed";
 import "./App.css";
+
+const App = () => {
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID="afebd063-551f-44ad-828c-d7cc303ca59c"
+      userName="Kota"
+      userSecret="123456"
+      renderChatFeed={chatAppProps => <ChatFeed {...chatAppProps} />}
+    />
+  );
+};
+
+export default App;
